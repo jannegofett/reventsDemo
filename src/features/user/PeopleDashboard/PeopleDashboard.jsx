0d34a8b5a5bdcb96efc3df1fcfd1d5c2751b787c
illarock <illarock3020@gmail.com>
2019-09-25 01:44:12 +0800

    End of section 6

---
+++ src/features/user/PeopleDashboard/PeopleDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PeopleDashboard = () => {
+  return (
+    <div>
+      <h1>People Dashboard Page</h1>
+      
+    </div>
+  )
+}
+
+export default PeopleDashboard
